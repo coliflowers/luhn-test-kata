32f7a0b2e5e6aa36f7e16c93442e306a99edff3b
lordudun <oriol@lordudun.es>
2017-05-17 20:39:12 +0200

    luhn-test a credit card with only one digit not zero is not a valid credit card

--- src/main/java/LunhTest.java
+++ src/main/java/LunhTest.java
@@ -3,6 +3,13 @@ public class LunhTest {
 
 
     public static boolean isValid(int digits) {
-        return true;
+
+        boolean res = false;
+
+        if (digits == 0) {
+            res = true;
+        }
+
+        return res;
     }
 }
--- src/test/java/LuhnTestTest.java
+++ src/test/java/LuhnTestTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class LuhnTestTest {
@@ -7,5 +8,10 @@ public class LuhnTestTest {
     @Test
     public void all_digits_zero() {
         assertTrue(LunhTest.isValid(00000000000));
+    }
+
+    @Test
+    public void all_zero_except_one_digit() {
+        assertFalse(LunhTest.isValid(00000000001));
     }
 }
